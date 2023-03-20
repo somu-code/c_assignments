a0cbee2920bf5a0bb06235c3ec5149d75e17c855
Somnath Golui <code.somu@gmail.com>
2023-03-20 15:21:23 +0530

    added gitignore and find_length_of_a_string program

---
+++ find_length_of_a_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[] = "Hello";
+	int count = 0;
+	char *p = str;
+	while (*p != '\0') {
+		count++;
+		p++;
+	}
+	printf("Length of string is: %d\n", count);
+	return 0;
+}
