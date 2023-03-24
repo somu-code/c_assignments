fe242de3902c02f303d719bccf82eb3cb956e23e
Somnath Golui <code.somu@gmail.com>
2023-03-24 15:58:23 +0530

    updated the code with pointers

--- reverse_elements_of_array.c
+++ reverse_elements_of_array.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-	int arr[10] = {0,1,2,3,4,5,6,7,8,9};
-	int reverse[10] = {0};
-	int i,j;
-	for (i = 9, j = 0; i >= 0; i--, j++) {
-		reverse[j] = arr[i];
-		printf("%d ", reverse[j]);
-	}
-	printf("\n");
-	return 0;
+void reverse(int* arr, int size) {
+    int *start = arr;
+    int *end = arr + size - 1;
+
+    while (start < end) {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int* arr = (int*)malloc(size * sizeof(int));
+    if (arr == NULL) {
+        printf("Failed to allocate memory. Exiting...\n");
+        return 1;
+    }
+
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", arr + i);
+    }
+
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));
+    }
+
+    reverse(arr, size);
+
+    printf("\nReversed array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));
+    }
+
+    printf("\n");
+
+    free(arr);
+    return 0;
 }
