732973faf7531224751d61956227e7e001f4743d
Somnath Golui <code.somu@gmail.com>
2023-03-30 22:36:51 +0530

    code works

---
+++ reading_a_line_at_a_time_from_a_file/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	FILE *fp;
+	char s[1024];
+	int linecount = 0;
+
+	fp = fopen("quote.txt", "r");
+
+	while (fgets(s, sizeof s, fp) != NULL)
+		printf("%d: %s", ++linecount, s);
+
+	fclose(fp);
+
+	return 0;
+}
