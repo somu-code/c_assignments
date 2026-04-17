98d09b2a9bc023a344f0eb46a1074fd55d6b0697
Somnath Golui <code.somu@gmail.com>
2026-04-17 08:48:20 +0530

    Number increaseing reverse pyramid pattern get printed.

---
+++ pattern_programs/number_increasing_reverse_pyramid.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 4; i > 0; i--) {
+        int j = 1;
+        for (int k = 1; k <= i; k++) {
+            printf("%d", j);
+            j++;
+        }
+        printf("\n");
+    }
+}
