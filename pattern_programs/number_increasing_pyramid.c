9edcfd9dd569da56761879422906dbda4704be60
Somnath Golui <code.somu@gmail.com>
2026-04-17 08:37:09 +0530

    Number increase pyramid pattern get printed

---
+++ pattern_programs/number_increasing_pyramid.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 0; i < 4; i++) {
+        int j = 1;
+        for (int k = 0; k <= i; k++) {
+            printf("%d", j);
+            j++;
+        }
+        printf("\n");
+    }
+}
