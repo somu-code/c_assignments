9b4e608941bd70c989f2114379171626b642775e
Somnath Golui <code.somu@gmail.com>
2026-04-17 08:56:33 +0530

    Fixed the spacing issue.

--- pattern_programs/number_increasing_pyramid.c
+++ pattern_programs/number_increasing_pyramid.c
@@ -4,7 +4,7 @@ int main(void) {
     for (int i = 0; i < 4; i++) {
         int j = 1;
         for (int k = 0; k <= i; k++) {
-            printf("%d", j);
+            printf("%d ", j);
             j++;
         }
         printf("\n");
