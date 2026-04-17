eeb0f7bff3b45913484f6a0b9f1fd1406fc36f33
Somnath Golui <code.somu@gmail.com>
2026-04-17 09:16:38 +0530

    Added a function to print pattern.

--- pattern_programs/number_changing_pyramid.c
+++ pattern_programs/number_changing_pyramid.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
+void print_pattern(int n) {
     int i = 1;
-    for (int j = 0; j < 4; j++) {
+    for (int j = 0; j < n; j++) {
         for (int k = 0; k <= j; k++) {
             printf("%d ", i);
             i++;
         }
         printf("\n");
     }
+}
+
+int main(void) {
+    int n = 4;
+    print_pattern(n);
 }
