3da89274a8fd6d89f1967a330a70e76747a1a5ca
Somnath Golui <code.somu@gmail.com>
2026-04-24 21:14:19 +0530

    pattern printed

---
+++ pattern_programs/hollow_reverse_triangle_pattern.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void print_pattern(int n) {
+    for (int i = 0; i < n; i++) {
+        if (i > 0) {
+            for (int j = 0; j < i; j++) {
+                printf(" ");
+            }
+        }
+        for (int j = n - i; j > 0; j--) {
+            if (i == 0 || i == n - 1) {
+                printf("* ");
+            } else if (j == 1 || j == n - i) {
+                printf("* ");
+            } else
+                printf("  ");
+            /* else if (j > 1 || j < n - i - 1) {
+                 printf("#");
+             }*/
+        }
+        printf("\n");
+    }
+}
+
+int main(void) {
+    int n = 5;
+    print_pattern(n);
+}
