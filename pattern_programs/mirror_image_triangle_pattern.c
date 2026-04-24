9202945e3341ce1254ca8203cd942622b5fdc490
Somnath Golui <code.somu@gmail.com>
2026-04-24 15:19:42 +0530

    pattern get printed but need to correct the spaces

---
+++ pattern_programs/mirror_image_triangle_pattern.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void print_pattern(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+            printf(" ");
+        }
+        for (int k = i; k <= n; k++) {
+            printf("%d ", k);
+        }
+        printf("\n");
+    }
+    for (int i = n - 1; i > 0; i--) {
+        /*        for (int j = i; j <= n; j++) {
+                    printf(" ");
+                }*/
+        for (int k = i; k <= n; k++) {
+            printf("%d ", k);
+        }
+        printf("\n");
+    }
+}
+
+int main(void) {
+    int n = 4;
+    print_pattern(n);
+}
