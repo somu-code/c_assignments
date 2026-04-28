1172794a4bb6f18887f0d8f7d8d4189e0824a371
Somnath Golui <code.somu@gmail.com>
2026-04-28 08:40:47 +0530

    printing a system error message when FILE pointer return NULL value

--- file_programming/read_one_character.c
+++ file_programming/read_one_character.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
     FILE *fp;
     int c;
     fp = fopen("hello.txt", "r");
     if (fp == NULL) {
-        printf("File does not exist\n");
-        return 0;
+        perror("fopen");
+        exit(1);
     }
     while ((c = fgetc(fp)) != EOF) {
         printf("%c", c);
