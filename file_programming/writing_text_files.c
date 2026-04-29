b93405707aea65fde668f8e648f03c1bfb7ebd6e
Somnath Golui <code.somu@gmail.com>
2026-04-29 11:49:25 +0530

    wrote to a file

---
+++ file_programming/writing_text_files.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int n = 0;
+    FILE* fp = fopen("output.txt", "w");
+    if (fp == NULL) {
+        perror("fopen");
+        exit(1);
+    }
+    fputc('A', fp);
+    fputc('\n', fp);
+    fputs("Hello, world!\n", fp);
+    fprintf(fp, "Address of int variable n %p\n", (void*)&n);
+    fclose(fp);
+}
