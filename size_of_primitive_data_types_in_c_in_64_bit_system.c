88a700aba563beeab3ba1a201bc4d7544d08b4cd
Somnath Golui <code.somu@gmail.com>
2026-05-03 18:54:33 +0530

    Size of primitive data types in my machine

---
+++ size_of_primitive_data_types_in_c_in_64_bit_system.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("size of char %zu\n", sizeof(char));
+    printf("size of short %zu\n", sizeof(short));
+    printf("size of unsigned short %zu\n", sizeof(unsigned short));
+    printf("size of int %zu\n", sizeof(int));
+    printf("size of float %zu\n", sizeof(float));
+    printf("size of long %zu\n", sizeof(long));
+    printf("size of long long %zu\n", sizeof(long long));
+    printf("size of double %zu\n", sizeof(double));
+    printf("size of long doulbe %zu\n", sizeof(long double));
+    printf("%d\n", 0x1234);
+}
