18e190ae716c5a4e1ffcaa031ec62f9e7755726a
Somnath Golui <code.somu@gmail.com>
2026-05-17 18:20:57 +0530

    Writes to file with -o flag, writes to stdout without it.

--- projects/markdown_parser/markdown_parser.c
+++ projects/markdown_parser/markdown_parser.c
@@ -3,19 +3,81 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+./mdparser                             # stdin → stdout
+./mdparser input.md                    # file → stdout
+./mdparser -o                          # stdin → auto generated filename
+./mdparser -o output.html              # stdin → named file
+./mdparser -o input.md                 # file → auto generated filename
+./mdparser -o output.html input.md     # file → named file
+*/
+
 int main(int argc, char **argv) {
-    if (argc > 2) {
-        printf("One markdown file at a time\n");
-        return EXIT_FAILURE;
-    }
     FILE *markdown_input = NULL;
+    FILE *output_html = NULL;
     if (argc == 1) {
+        // ./a.out  stdin -> stdout
         markdown_input = stdin;
-    }
-    if (argc == 2) {
-        markdown_input = fopen(argv[1], "r");
+        output_html = stdout;
+    } else if (argc == 2) {
+        if (strrchr(argv[1], '.') != NULL &&
+            strcmp(strrchr(argv[1], '.'), ".md") == 0) {
+            // ./a.out example.md   file -> stdout
+            markdown_input = fopen(argv[1], "r");
+            if (markdown_input == NULL) {
+                perror("Error reading file");
+                return EXIT_FAILURE;
+            }
+            output_html = stdout;
+        } else if (argv[1][0] == '-' && argv[1][1] == 'o') {
+            // ./a.out -o   stdin -> auto generated filename
+            markdown_input = stdin;
+            output_html = fopen("output.html", "w");
+            if (output_html == NULL) {
+                perror("Error writing to file");
+                return EXIT_FAILURE;
+            }
+        }
+    } else if (argc == 3) {
+        if (argv[1][0] == '-' && argv[1][1] == 'o' &&
+            strrchr(argv[2], '.') != NULL &&
+            strcmp(strrchr(argv[2], '.'), ".html") == 0) {
+            // ./a.out -o example.html   stdin -> named file
+            markdown_input = stdin;
+            output_html = fopen(argv[2], "w");
+            if (output_html == NULL) {
+                perror("Error writing to file");
+                return EXIT_FAILURE;
+            }
+        } else if (argv[1][0] == '-' && argv[1][1] == 'o' &&
+                   strrchr(argv[2], '.') != NULL &&
+                   strcmp(strrchr(argv[2], '.'), ".md") == 0) {
+            // ./a.out -o example.md    file -> auto generated filename
+            markdown_input = fopen(argv[2], "r");
+            if (markdown_input == NULL) {
+                perror("Error reading file");
+                return EXIT_FAILURE;
+            }
+            output_html = fopen("output.html", "w");
+            if (output_html == NULL) {
+                perror("Error writing to file");
+                return EXIT_FAILURE;
+            }
+        }
+    } else if (argc == 4 && argv[1][0] == '-' && argv[1][1] == 'o' &&
+               strrchr(argv[2], '.') != NULL &&
+               strcmp(strrchr(argv[2], '.'), ".html") == 0 &&
+               strrchr(argv[3], '.') != NULL &&
+               strcmp(strrchr(argv[3], '.'), ".md") == 0) {
+        // ./a.out -o output.html example.md
+        markdown_input = fopen(argv[3], "r");
         if (markdown_input == NULL) {
-            perror("FILE pointer is NULL");
+            perror("Error reading file");
+            return EXIT_FAILURE;
+        }
+        output_html = fopen(argv[2], "w");
+        if (output_html == NULL) {
+            perror("Error writing to file");
             return EXIT_FAILURE;
         }
     }
@@ -44,35 +106,37 @@ int main(int argc, char **argv) {
             if (p != NULL) {
                 *p = '\0';
             }
-            printf("<h%zu>%s</h%zu>\n", number_of_headings,
-                   &line_buffer[number_of_headings + 1], number_of_headings);
-        } else if (!in_unordered_list && line_buffer[0] == '-') {
+            fprintf(output_html, "<h%zu>%s</h%zu>\n", number_of_headings,
+                    &line_buffer[number_of_headings + 1], number_of_headings);
+        } else if (!in_unordered_list && line_buffer[0] == '-' &&
+                   line_buffer[1] != '-') {
             in_unordered_list = true;
-            printf("<ul>\n");
+            fprintf(output_html, "<ul>\n");
             char *p = strchr(line_buffer, '\n');
             if (p != NULL) {
                 *p = '\0';
             }
-            printf("<li>%s</li>\n", &line_buffer[2]);
-        } else if (in_unordered_list && line_buffer[0] == '-') {
+            fprintf(output_html, "<li>%s</li>\n", &line_buffer[2]);
+        } else if (in_unordered_list && line_buffer[0] == '-' &&
+                   line_buffer[1] != '-') {
             char *p = strchr(line_buffer, '\n');
             if (p != NULL) {
                 *p = '\0';
             }
-            printf("<li>%s</li>\n", &line_buffer[2]);
+            fprintf(output_html, "<li>%s</li>\n", &line_buffer[2]);
         } else if (in_unordered_list) {
             in_unordered_list = false;
-            printf("</ul>\n");
+            fprintf(output_html, "</ul>\n");
         } else if (!in_code_block && line_buffer[0] == '`' &&
                    line_buffer[1] == '`' && line_buffer[2] == '`') {
             in_code_block = true;
-            printf("<pre><code>\n");
+            fprintf(output_html, "<pre><code>\n");
         } else if (in_code_block && line_buffer[0] == '`' &&
                    line_buffer[1] == '`' && line_buffer[2] == '`') {
             in_code_block = false;
-            printf("</code></pre>\n");
+            fprintf(output_html, "</code></pre>\n");
         } else if (in_code_block) {
-            printf("%s", line_buffer);
+            fprintf(output_html, "%s", line_buffer);
         }
     }
     free(line_buffer);
